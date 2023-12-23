de2357fe14af359cb75060b49a6692baaa81da52
Shrihari Magar <shriharimagar1@gmail.com>
2023-12-23 10:51:11 +0530

    screensize adjusted

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,11 +45,10 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-    
+            
     def _check_keyup_events(self, event):
         """Respond to key releases."""
         if event.key == pygame.K_RIGHT:
-            # Move the ship to the right.
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
