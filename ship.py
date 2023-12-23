267ad414f7f32c08cc68ae3952e99a8344b58b08
Shrihari Magar <shriharimagar1@gmail.com>
2023-12-23 10:30:48 +0530

    ship speed increase

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen # Assigning the screen to an attribute of Ship
+        self.settings = ai_game.settings # Assigning the settings to an attribute of Ship
         self.screen_rect = ai_game.screen.get_rect() # Getting the screen's rect attribute
         
         # Load the ship image and get its rect.
@@ -15,6 +16,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom # Setting the ship's rect's midbottom attribute to match the screen's midbottom attribute
 
+        # Store a float value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -23,10 +27,14 @@ class Ship:
     
     def update(self):
         """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right: # Checking if the ship's right attribute is less than the screen's right attribute
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x.
+        self.rect.x = self.x
     
     def blitme(self):
         """Draw the ship at its current location."""
