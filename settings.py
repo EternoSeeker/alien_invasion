03bb7cc2f52fe7a65b46a9c247b6117da5050f71
Shrihari Magar <shriharimagar1@gmail.com>
2023-12-25 09:55:21 +0530

    bullet settings

--- settings.py
+++ settings.py
@@ -3,6 +3,12 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60 ,60)
+        
         # Screen settings
 
         self.screen_width = 1100
