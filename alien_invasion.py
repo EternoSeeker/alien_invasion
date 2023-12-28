a784089811dd497e1076de17b0af054b77929808
Shrihari Magar <shriharimagar1@gmail.com>
2023-12-28 11:48:42 +0530

    settings update

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,7 +114,18 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
     
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def update_aliens(self):
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Bullet settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 3
+        self.bullet_speed = 2.7
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60 ,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 4
 
         # Screen settings
 
@@ -17,10 +17,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 1.8
 
         # Alien settings
-        self.alien_speed = 1.0
+        self.alien_speed = 1.1
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
