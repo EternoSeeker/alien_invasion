8dc9eb96b4e735cc243b9b35f9b0921d9673bfa5
Shrihari Magar <shriharimagar1@gmail.com>
2023-12-28 11:21:57 +0530

    fleet movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,6 +44,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -52,7 +65,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
-
+            self.update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -101,6 +114,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    
+    def update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
